3277322bbf1f0549240639d8d42d5062c2b0cb8b
Jozsef <pallagijoe@gmail.com>
2017-08-11 07:55:41 +0200

    deleted unnecessary lines

--- src/com/Nadir/Digital/JeuDeLaVie/Model/GameOfLife.java
+++ src/com/Nadir/Digital/JeuDeLaVie/Model/GameOfLife.java
@@ -11,32 +11,28 @@ public class GameOfLife implements Observable{
 	
 	//la list des observateurs qui observe notre model
 	private ArrayList<Observer> listObservers = new ArrayList<Observer>();
-	// une liste de cellule celle qui est utilsé dans notre algortithme 
+	// une liste de cellule celle qui est utilsÃ© dans notre algortithme 
 	
 	private ArrayList<Cell> cells ;
 	
-
-	
 	/**
-	 * la classe à un seul constructeur 
+	 * la classe Ã  un seul constructeur 
 	 * son role est d'initialiser la matrice avec une petit droite 
 	 * NB: une cellule vivante prend la valeur 1 sinon 0
 	 * */
 	public GameOfLife() {
 		cells = new ArrayList<Cell>();	
-		
-		
 	}
+	
 	/**
-	 * cette méthod calcule le nombre de voisin d'une cellule , 
-	 * elle applique les règles des jeux , en ajoutant cette cellule a notre liste soit avec une état mourante ou naissance 
+	 * cette mÃ©thod calcule le nombre de voisin d'une cellule , 
+	 * elle applique les rÃ¨gles des jeux , en ajoutant cette cellule a notre liste soit avec une Ã©tat mourante ou naissance 
 	 * @param i position de la cellule dans une colonne 
 	 * @param j position de la cellule dans une ligne
 	 */
 	public void  calculNbrNeighbours(int i ,int j){
 		int cellNbr = 0;
-		try{
-			
+		try{		
 			// deux boucle pour calculer le nbr de cellule vivantes dans le voisinage 
 			for(int k=(i==0)?0:-1;(i==(SIZE-1))?k<1:k<=1;k++){
 				for(int l=(j==0)?0:-1;(j==(SIZE-1))?l<1:l<=1;l++){
@@ -49,38 +45,32 @@ public class GameOfLife implements Observable{
 				}
 			}
 			
-			
-			//application des règles naissance si 3 , survivre si 2 ou 3 
-			
+			//application des rÃ¨gles naissance si 3 , survivre si 2 ou 3 	
 			if(cellNbr<2||cellNbr>3 && matrix[i][j] == 1){
 				cells.add(new Cell(i, j, false));
 			}else if(cellNbr == 3 && matrix[i][j] == 0){
 				cells.add(new Cell(i, j, true));
 			}
-			
 		}catch(ArrayIndexOutOfBoundsException e){
 			e.printStackTrace();
 		}	
 	}
 	
-	
 	/**
-	 * la méthode nextGeneration applique  l'algorithme 
+	 * la mÃ©thode nextGeneration applique  l'algorithme 
 	 * 
 	 * */
 	public void nextGeneration(){
 		// initialisation a liste 
 		cells = new ArrayList<Cell>();
-		
 		// pour chaque on calcule le nbr de voisins 
 		for(int i=0;i<SIZE;i++){
 			for(int j=0;j<SIZE;j++){
 				calculNbrNeighbours(i, j);			
 			}
-			
 		}
 		
-		// pour chaque cellule modifié on fait les modification nécéssaire 
+		// pour chaque cellule modifiÃ© on fait les modification nÃ©cÃ©ssaire 
 		for (Cell cell : cells) {
 			if(cell.isBorn()){
 				matrix[cell.getI()][cell.getJ()] = 1;		
@@ -89,7 +79,6 @@ public class GameOfLife implements Observable{
 			}
 		}
 		updateObserver();
-		
 	}
 
 	
@@ -106,13 +95,12 @@ public class GameOfLife implements Observable{
 	 * */
 	
 	public void setMatrix(int[][] matrix) {
-		
 		this.matrix = matrix;
 		updateObserver();
 	}
 	
 	/**
-	 * la méthode creatCell cree ou tu une cellule 
+	 * la mÃ©thode creatCell cree ou tu une cellule 
 	 * 
 	 * @param i position de la cellule dans une colonne 
 	 * @param j position de la cellule dans une ligne
@@ -126,14 +114,10 @@ public class GameOfLife implements Observable{
 			else if(type == 3)
 				matrix[i][j] = 0;
 		} catch (IndexOutOfBoundsException e) {
-			
 		}
 		updateObserver();
 	}
-	
-	
-	
-	
+
 	/**
 	 * ajout d'un observateur a notre liste d'observateur
 	 * @param obs : Observateur 
@@ -159,7 +143,6 @@ public class GameOfLife implements Observable{
 	 * */
 	@Override
 	public void deletObserver() {
-		listObservers = new ArrayList<Observer>();
-		
+		listObservers = new ArrayList<Observer>();		
 	}
 }
